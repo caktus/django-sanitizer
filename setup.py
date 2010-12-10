3f7f688a3addc4ac93adb0b834315404bffaa700
Calvin Spealman <calvin@caktusgroup.com>
2010-12-10 10:38:05 -0500

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+
+setup(name="django-sanitizer",
+      version="0.1",
+      description="Django template filter application for sanitizing user submitted HTML",
+      author="Calvin Spealman",
+      url="http://github.com/caktus/django-sanitizer",
+      packages=['sanitizer'],
+      )
