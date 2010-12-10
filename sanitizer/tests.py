7eb0c88484022846ef261959abb3d8a736056884
Calvin Spealman <calvin@caktusgroup.com>
2010-12-10 10:25:05 -0500

    Add full tests to a variety of conditions under which HTML needs to be sanitized.

--- sanitizer/tests.py
+++ sanitizer/tests.py
@@ -7,17 +7,42 @@ Replace these with more appropriate tests for your application.
 
 from django.test import TestCase
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.failUnlessEqual(1 + 1, 2)
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+from sanitizer.templatetags.sanitizer import allowtags
 
->>> 1 + 1 == 2
-True
-"""}
+class SanitizerTest(TestCase):
 
+    def test_remove_not_allowed(self):
+
+        cleaned = allowtags("<a></a><b></b>", "a")
+        self.assertEqual(cleaned, "<a></a>")
+
+    def test_remove_nested_not_allowed(self):
+    
+        cleaned = allowtags("<a><b></b></a>", "a")
+        self.assertEqual(cleaned, "<a></a>")
+
+    def test_remove_outer_not_allowed(self):
+    
+        cleaned = allowtags("<a><b></b></a>", "b")
+        self.assertEqual(cleaned, "<b></b>")
+
+    def test_allow_multiple(self):
+    
+        cleaned = allowtags("<a></a><b></b><c></c>", "a c")
+        self.assertEqual(cleaned, "<a></a><c></c>")
+
+    def test_allow_specific_attribute(self):
+    
+        cleaned = allowtags('<a b="x" c="y"></a>', "a:b")
+        self.assertEqual(cleaned, '<a b="x"></a>')
+
+    def test_distinguish_attribute_allowances(self):
+    
+        cleaned = allowtags('<a b="x" c="y"></a><d b="x" c="y"></d>', "a:b d:c")
+        self.assertEqual(cleaned, '<a b="x"></a><d c="y"></d>')
+
+    def test_keep_contents_of_disallowed(self):
+    
+        cleaned = allowtags("<blink>test</blink>", "")
+        self.assertEqual(cleaned, 'test')
