355d71bb600df850b3914772d0dca9e0a68e64c8
Calvin Spealman <calvin@caktusgroup.com>
2012-03-14 13:50:11 -0400

    Make caktus the owner, listing myself as a maintainer.

--- setup.py
+++ setup.py
@@ -4,9 +4,11 @@ from distutils.core import setup
 
 
 setup(name="django-sanitizer",
-      version="0.3",
+      version="0.4",
       description="Django template filter application for sanitizing user submitted HTML",
-      author="Calvin Spealman",
+      author="Caktus Consulting Group",
+      maintainer="Calvin Spealman",
+      maintainer_email="calvin@caktusgroup.com",
       url="http://github.com/caktus/django-sanitizer",
       packages=['sanitizer', 'sanitizer.templatetags'],
       )
